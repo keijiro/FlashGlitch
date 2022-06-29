5d9cfb090f01145d8f9ecac5323f2ff391d4872d
Keijiro Takahashi <keijiro@gmail.com>
2022-06-29 18:32:51 +0900

    Update C# implementation

---
+++ Assets/FlashGlitchController.cs
@@ -0,0 +1,71 @@
+using UnityEngine;
+
+namespace FlashGlitch {
+
+public sealed class FlashGlitchController : MonoBehaviour
+{
+    #region Editable attributes
+
+    [SerializeField] float _releaseTime1 = 0.5f;
+    [SerializeField] float _releaseTime2 = 0.5f;
+    [SerializeField] uint _seed = 0;
+    [SerializeField] float _hue = 0;
+
+    #endregion
+
+    #region Public method
+
+    public void TriggerEffect(int index, float value)
+    {
+        if (index == 0) _value1 = value; else _value2 = value;
+    }
+
+    public void TriggerEffect1(float value)
+      => _value1 = value;
+
+    public void TriggerEffect2(float value)
+      => _value2 = value;
+
+    public void RandomizeSeed()
+      => _seed = (uint)Random.Range(0, 100000);
+
+    public void RandomizeHue()
+      => _hue = Random.value;
+
+    #endregion
+
+    #region Private members
+
+    MaterialPropertyBlock _block;
+    float _value1, _value2;
+
+    #endregion
+
+    #region MonoBehaviour implementation
+
+    void Start()
+      => _block = new MaterialPropertyBlock();
+
+    void Update()
+    {
+        _value1 = Mathf.Max(0, _value1 - Time.deltaTime / _releaseTime1);
+        _value2 = Mathf.Max(0, _value2 - Time.deltaTime / _releaseTime2);
+    }
+
+    void LateUpdate()
+    {
+        var renderer = GetComponent<Renderer>();
+        renderer.GetPropertyBlock(_block);
+
+        _block.SetFloat("_Param1", _value1);
+        _block.SetFloat("_Param2", _value2);
+        _block.SetFloat("_Seed", _seed);
+        _block.SetFloat("_HueShift", _hue);
+
+        renderer.SetPropertyBlock(_block);
+    }
+
+    #endregion
+}
+
+} // namespace FlashGlitch
--- Assets/RandomTrigger.cs
+++ Assets/RandomTrigger.cs
@@ -1,17 +1,29 @@
 using UnityEngine;
 using System.Collections;
 
-sealed class RandomTrigger : MonoBehaviour
+namespace FlashGlitch {
+
+public sealed class RandomTrigger : MonoBehaviour
 {
-    [SerializeField] FlashGlitch _target = null;
+    [SerializeField] FlashGlitchController _target = null;
     [SerializeField] int _index = 0;
+    [SerializeField] float _minValue = 0;
+    [SerializeField] float _maxValue = 1;
 
     IEnumerator Start()
     {
         while (true)
         {
-            yield return new WaitForSeconds(Mathf.Pow(0.5f, Random.Range(1, 4)));
-            _target.TriggerEffect(_index);
+            var time = Mathf.Pow(0.5f, Random.Range(1, 4));
+            yield return new WaitForSeconds(time);
+
+            var value = Random.Range(_minValue, _maxValue);
+            _target.TriggerEffect(_index, value);
+
+            _target.RandomizeSeed();
+            if (Random.value < 0.01f) _target.RandomizeHue();
         }
     }
 }
+
+} // namespace FlashGlitch
