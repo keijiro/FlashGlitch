e9e5fb7ec41b68b9ab6e67462e22e7629059a117
Keijiro Takahashi <keijiro@gmail.com>
2024-09-12 23:02:28 +0900

    HDRP support

--- Packages/jp.keijiro.flashglitch/Scripts/FlashGlitchController.cs
+++ Packages/jp.keijiro.flashglitch/Scripts/FlashGlitchController.cs
@@ -6,10 +6,10 @@ public sealed class FlashGlitchController : MonoBehaviour
 {
     #region Editable attributes
 
-    [SerializeField] float _releaseTime1 = 0.5f;
-    [SerializeField] float _releaseTime2 = 0.5f;
-    [SerializeField] uint _seed = 0;
-    [SerializeField] float _hue = 0;
+    [field:SerializeField] public float ReleaseTime1 { get; set; } = 0.5f;
+    [field:SerializeField] public float ReleaseTime2 { get; set; } = 0.5f;
+    [field:SerializeField] public uint Seed { get; set; } = 0;
+    [field:SerializeField] public float Hue { get; set; } = 0;
 
     #endregion
 
@@ -27,10 +27,10 @@ public sealed class FlashGlitchController : MonoBehaviour
       => _value2 = value;
 
     public void RandomizeSeed()
-      => _seed = (uint)Random.Range(0, 100000);
+      => Seed = (uint)Random.Range(0, 100000);
 
     public void RandomizeHue()
-      => _hue = Random.value;
+      => Hue = Random.value;
 
     public Texture Source { get; set; }
 
@@ -50,8 +50,8 @@ public sealed class FlashGlitchController : MonoBehaviour
 
     void Update()
     {
-        _value1 = Mathf.Max(0, _value1 - Time.deltaTime / _releaseTime1);
-        _value2 = Mathf.Max(0, _value2 - Time.deltaTime / _releaseTime2);
+        _value1 = Mathf.Max(0, _value1 - Time.deltaTime / ReleaseTime1);
+        _value2 = Mathf.Max(0, _value2 - Time.deltaTime / ReleaseTime2);
     }
 
     void LateUpdate()
@@ -62,8 +62,8 @@ public sealed class FlashGlitchController : MonoBehaviour
         if (Source != null) _block.SetTexture("_MainTex", Source);
         _block.SetFloat("_Effect1", _value1);
         _block.SetFloat("_Effect2", _value2);
-        _block.SetFloat("_Seed", _seed);
-        _block.SetFloat("_Hue", _hue);
+        _block.SetFloat("_Seed", Seed);
+        _block.SetFloat("_Hue", Hue);
 
         renderer.SetPropertyBlock(_block);
     }
